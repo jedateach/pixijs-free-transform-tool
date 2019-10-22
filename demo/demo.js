6d2405e318603d57bd7a1d797d6589a2d98d6df4
Jeremy Shipman <jeremy@burnbright.net>
2019-10-23 07:41:11 +1300

    Refatored demo.js to constrain objects on resize, share clickToSelect functionality

--- demo/demo.js
+++ demo/demo.js
@@ -1,19 +1,19 @@
 (function(){
 
-var canvas, app, stage;
+var canvas, app, stage, container;
 
 var boundary;
 var boundaryLine;
 
 var selectTool;
 
-var container;
-
 // Resize function window
 function resize() {
+	selectTool.unselect();
 	const parent = app.view.parentNode;
-	app.renderer.resize(parent.clientWidth, parent.clientHeight);
+	app.renderer.resize(parent.clientWidth, app.renderer.height);
 	updateBoundary(boundary);
+	constrainStageObjects(container.children, boundary);
 }
 
 function updateBoundary(boundary) {
@@ -24,8 +24,13 @@ function updateBoundary(boundary) {
 	boundary.y = padding;
 	boundary.width = canvas.width - padding * 2;
 	boundary.height = canvas.height - padding * 2;
-
 	drawBoundaryLine();
+}
+
+function constrainStageObjects(objects, container) {
+	objects.forEach(function(obj) {
+		PIXI.util.constrainObjectTo(obj, container);
+	});
 }
 
 function drawBoundaryLine() {
@@ -33,8 +38,17 @@ function drawBoundaryLine() {
 	boundaryLine.lineWidth = 1;
 	boundaryLine.lineColor = "#000";
 	boundaryLine
-		.lineStyle(2, 0x000000, 1)
+		.lineStyle(1, 0x000000, 1)
 		.drawRect(boundary.x, boundary.y, boundary.width, boundary.height);
+}
+
+function clickToSelect(obj) {
+	obj.interactive = true;
+	obj.cursor = "pointer";
+	obj.on("pointertap", function (evt) {
+		evt.stopPropagation();
+		selectTool.select(evt.currentTarget);
+	});
 }
 
 function init() {
@@ -48,10 +62,8 @@ function init() {
 	});
 	stage = app.stage;
 
-
 	// Listen for window resize events
 	window.addEventListener('resize', resize);
-
 
 	// set up free transform tool
 	const top = new PIXI.Container();
@@ -71,77 +83,91 @@ function init() {
 
 	top.addChild(selectTool);
 	
-	function clickToSelect(obj) {
-		obj.interactive = true;
-		obj.cursor = "pointer";
-		obj.on("pointertap", function (evt) {
-			evt.stopPropagation();
-			selectTool.select(evt.currentTarget);
-			update = true;
-		});
-	}
-
-	// function constrainStageObjects(objects) {
-	// 	objects.forEach(function(obj) {
-	// 		createjs.util.constrainObjectTo(obj, boundary);
-	// 	});
-	// }
-
 	container = new PIXI.Container();
 	stage.addChildAt(container, 0);
 
+	// app.renderer.plugins.interaction.on("pointerup", function(e) {
+	// 	console.log("click ", e.data.global.x, e.data.global.y);
+	// });
+
+	let ellipse = createEllipse();
+	container.addChild(ellipse);
+
+	let image = createBitmap('demo/flower.svg');
+	container.addChild(image);
+	
+	let text = createText("Off\nCentre");
+	container.addChild(text);
+
+
+	// TODO: figure out how to animate handle dragging?
+	// app.ticker.add(function(delta) {
+	// 	text.rotation -= 0.01 * delta;
+	// 	text.position.x += delta + 1;
+	// 	ellipse.rotation += 0.05 * delta;
+	// 	ellipse.position.x += delta + (Math.random() - 0.5) * 10;
+	// 	ellipse.position.y += delta + (Math.random() - 0.5) * 10;
+	// 	image.rotation += 0.03 * delta;
+	// 	resize();
+	// });
+	
+	resize();
+}
+
+function createEllipse() {
 	// Shape
 	var ellipse = new PIXI.Graphics();
-	ellipse.x = (canvas.width / 4);
+	ellipse.x = canvas.width / 4;
 	ellipse.y = canvas.height / 4;
-	ellipse.pivot.set(
-		ellipse.getBounds().width / 2 | 0,
-		ellipse.getBounds().height / 6 | 0
-	);
 	ellipse
 		.beginFill(0x3355EE)
-		// .setStrokeStyle(4)
-		// .beginRadialGradientFill(["#FFF","#35E"],[1,0],0,0,200,30,-50,40)
+		.lineStyle(10, 0x000)
 		.drawEllipse(0, 0, 50, 100);
-	clickToSelect(ellipse);
-	container.addChild(ellipse);
-	ellipse.scale.set(1.5, 1);
 
+	// TODO: figure out pivot
+	// ellipse.pivot.x = ellipse.width * 0.1;
+	// ellipse.pivot.y = ellipse.height * 0.1;
+	clickToSelect(ellipse);
+	return ellipse;
+}
+
+function createBitmap(filename) {
 	// Bitmap
-	const texture = PIXI.Texture.from('demo/flower.svg');
+	const texture = PIXI.Texture.from(filename);
 	const bitmap = new PIXI.Sprite(texture);
 	bitmap.position.set(canvas.width / 2, canvas.height / 6);
 	bitmap.anchor.set(0.5);
 	bitmap.rotation = -25 | 0;
 	clickToSelect(bitmap);
-	container.addChild(bitmap);
-		
-	// Text
+	return bitmap;
+}
+
+function createText(value) {
 	let style = new PIXI.TextStyle({
-		fontFamily: "Arial",
+		align: "center",
+		fontFamily: "Century Gothic",
 		fontSize: 86,
-		fill: "white",
-		stroke: '#ff3300',
-		strokeThickness: 4,
+		fontWeight: "bold",
+		fill: ["navy", "black"],
+		fillGradientType: PIXI.TEXT_GRADIENT.LINEAR_VERTICAL,
+		// fillGradientStops: 
+		stroke: '#fff',
+		strokeThickness: 6,
 		dropShadow: true,
 		dropShadowColor: "#000000",
+		dropShadowAlpha: 0.4,
 		dropShadowBlur: 4,
 		dropShadowAngle: Math.PI / 6,
 		dropShadowDistance: 6,
+		lineJoin: "round"
 	});
-	var text = new PIXI.Text("Hello\nWorld", style);
-	text.position.set(canvas.width / 2, canvas.height / 2.3);
-	text.rotation = 5 | 0;
-	text.scale.set(0.5);
-
-	text.anchor.set(0.5); // TODO: allow non-centered anchor
-
+	var text = new PIXI.Text(value, style);
+	text.position.set(canvas.width, canvas.height / 2.3);
+	text.rotation = 0;
+	// text.scale.set(1);
+	text.anchor.set(0.2); // TODO: allow non-centered anchor
 	clickToSelect(text);
-	container.addChild(text);
-	
-	// TODO: add a container of shapes
-
-	resize();
+	return text;
 }
 
 init();
